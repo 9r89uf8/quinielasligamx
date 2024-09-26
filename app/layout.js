0d502038ddc6ffd6dd07fea19b131acf0fb07765
unknown <silvanovaldez53@outlook.com>
2024-09-26 14:58:44 -0500

    dkfj

--- app/layout.js
+++ app/layout.js
@@ -10,11 +10,11 @@ function generateSchemaMarkup() {
         "@context": "https://schema.org",
         "@type": "WebSite",
         "name": "quiniela liga mx",
-        "url": "https://www.quinielasligamx.com",
+        "url": "https://www.quinielaligamx.com",
         "description": "Quiniel liga mx 2024-2025 del fútbol mexicano. 10,000 dólares en premios",
         "potentialAction": {
             "@type": "SearchAction",
-            "target": "https://www.quinielasligamx.com/search?q={search_term_string}",
+            "target": "https://www.quinielaligamx.com/search?q={search_term_string}",
             "query-input": "required name=search_term_string"
         }
     };
@@ -31,10 +31,10 @@ const Layout = ({ children }) => {
               <title>Quiniela liga mx 2024-2025 - 10,000 Dólares en Efectivo a los Ganadores.</title>
               <meta name="description" content="10,000 dólares en premios. Participa en las mejor quiniela Liga MX 2024-2025 con equipos como América, Chivas, y Cruz Azul. ¡Únete a la quiniela Liga MX más emocionante!" />
               <meta name="keywords" content="quinielas liga mx 2024, quinielas liga mx, quiniela liga mx, quiniela liga mx 2024, apuestas deportivas" />
-              <link rel="canonical" href="https://www.quinielasligamx.com/" />
+              <link rel="canonical" href="https://www.quinielaligamx.com/" />
               <meta property="og:title" content="Quinielas Liga MX 2024 | Gana $10,000 en Efectivo"/>
               <meta property="og:description" content="Participa en las mejores quinielas Liga MX 2024-2025. Gana hasta $10,000 en efectivo prediciendo resultados. ¡Únete ahora!"/>
-              <meta property="og:url" content="https://www.quinielasligamx.com/"/>
+              <meta property="og:url" content="https://www.quinielaligamx.com/"/>
               <meta property="og:type" content="website"/>
               <meta name="twitter:card" content="summary_large_image"/>
               <meta name="twitter:title" content="Quinielas Liga MX 2024 | Gana $10,000 en Efectivo"/>

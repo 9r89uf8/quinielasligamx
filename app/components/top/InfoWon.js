bfef0da45e0c0c0f6cc9566c7dca8a44f9589618
unknown <silvanovaldez53@outlook.com>
2024-11-20 20:56:13 -0600

    kj

--- app/components/top/InfoWon.js
+++ app/components/top/InfoWon.js
@@ -6,6 +6,7 @@ import PhoneIcon from '@mui/icons-material/Phone';
 import {styled} from "@mui/material/styles";
 import Paper from "@mui/material/Paper"; // Adjust the import path as necessary
 import Button from '@mui/material/Button';
+import WinningsReceptionMethods from "@/app/components/WinningsReceptionMethods";
 import Box from '@mui/material/Box';
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -13,6 +14,7 @@ const Item = styled(Paper)(({ theme }) => ({
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary,
+    background: 'linear-gradient(135deg, #343a40, #212529)',
     margin:'12px auto 2px auto'
 }));
 const InfoWon = ({ user }) => {
@@ -20,38 +22,28 @@ const InfoWon = ({ user }) => {
     return (
         <Grid item sm={11} lg={10} xs={11}>
             <Item elevation={6}>
-                <Typography variant="h4" component="h2" gutterBottom style={{ color: '#333', fontFamily: '"Roboto", sans-serif' }}>
+                <Typography variant="h4" component="h2" gutterBottom style={{ color: '#ffffff', fontFamily: '"Roboto", sans-serif' }}>
                     Cantidad Ganada
                 </Typography>
                 <Divider>
                     <SavingsIcon style={{margin: 9}}/>
                 </Divider>
                 {user && user.amountWon ?
-                    <Typography component="div" variant="h4" color="text.primary">
+                    <Typography component="div" variant="h4" style={{color: '#ffffff'}}>
                         ${user.amountWon} {user.country==='US'?<span>Dólares</span>:<span>Pesos</span>}
                     </Typography>
                     :
-                    <Typography component="div" variant="h3" color="text.primary">
+                    <Typography component="div" variant="h3" style={{color: '#ffffff'}}>
                         $0.00
                     </Typography>
                 }
-                <div style={{border: '2px blue solid', borderRadius: 5, marginTop: 10}}>
-                    <Typography component="div" variant="h5" color="text.primary" style={{margin: 12, color: "red"}}>
-                        Anuncio importante
-                    </Typography>
-                    <Typography component="div" variant="h5" color="text.primary" style={{textAlign: "start", margin: 12}}>
-                        uno de nuestros representantes se comunicará con usted si gana por número de teléfono.
-                    </Typography>
-                    <Divider variant="middle" />
-                    <Typography component="div" variant="h5" color="text.primary" style={{textAlign: "start", margin: 12}}>
-                        si no podemos comunicarnos con usted por teléfono, le enviaremos un mensaje de texto y un correo electrónico.
-                    </Typography>
-                </div>
+
+                <WinningsReceptionMethods/>
 
                 {user ?
                     <div style={{marginTop: 12}}>
-                        <Typography component="div" variant="h5" color="text.primary" style={{color: "blue"}}>
-                            <PhoneIcon style={{margin:'0px 10px -4px auto'}}/>{user.phone}
+                        <Typography component="div" variant="h5" color="text.primary" style={{color: '#ffffff'}}>
+                            <PhoneIcon style={{margin:'0px 10px -4px auto', color: '#ffffff'}}/>{user.phone}
                         </Typography>
                     </div>
                     :
@@ -60,7 +52,7 @@ const InfoWon = ({ user }) => {
 
                 {showForm && user?
                     <Grid item sm={12} lg={12} xs={12}>
-                        <Button style={{margin: '10px auto 25px auto'}} variant="contained" color="warning" onClick={() => setShowForm(!showForm)}>Cancelar</Button>
+                        <Button style={{margin: '10px auto 25px auto', color: '#ffffff'}} variant="contained" color="warning" onClick={() => setShowForm(!showForm)}>Cancelar</Button>
                     </Grid>
                     :
                     user?
